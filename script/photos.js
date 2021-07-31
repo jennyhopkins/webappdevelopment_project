af18c999a15567fa015506feaac4830814bd2146
Jenny Hopkins <Jenny@Jennys-Air.fios-router.home>
2021-07-30 23:29:48 -0400

    added home page, gallery page with enlarge on hover

---
+++ script/photos.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+
+    Array.prototype.forEach.call(document.querySelector("img"), function (elem) {
+    elem.addEventListener("click", function () {
+        elem.classList.toggle("enlarged");
+    });
+});
+
+</script>
